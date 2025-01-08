187315bc957efdf980b98bb58a15d8c776a58aa4
SaiTejaswarDev <77905606+SaiTejaswarDev@users.noreply.github.com>
2025-01-08 12:54:09 -0600

    Added Gateway Server to the microservices architecture.

---
+++ gatewayserver/src/test/java/com/saitejaswar/gatewayserver/GateWayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saitejaswar.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateWayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
